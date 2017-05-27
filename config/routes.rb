e20aacf640385ef0aed7f0b2a9c97a66270d4333
Chris <codemedarkone@gmail.com>
2017-05-27 17:04:47 -0600

    implemented custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+
+  get 'home', to: 'pages#home'
+
+  get 'profile', to: 'pages#profile'
+  get 'explore', to: 'pages#explore'
+
+  root to: 'pages#index'
 end
