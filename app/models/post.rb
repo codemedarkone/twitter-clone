65782318ed4aba0292e78605a7e0c9312db25515
Chris <codemedarkone@gmail.com>
2017-05-27 21:32:23 -0600

    created post model and associations with users

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+  belongs_to :user
+
+  validates_presence_of :user_id 
+  validates :content, length: { maximum: 140 } # tweets are capped at 140 characters
+
+  default_scope -> {order(create_at: :desc) } # newest tweets /post first
+end
